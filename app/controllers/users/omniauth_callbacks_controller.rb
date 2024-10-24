3ed4b25e06f180c352bb7cc8d70b2eb28105c3ca
Kazuya-53b <157182514+Kazuya-53b@users.noreply.github.com>
2024-10-24 18:09:37 +0900

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"])
-  
+
     if @user.persisted?
       flash[:notice] = "ログインしました"
       sign_in_and_redirect @user, event: :authentication
